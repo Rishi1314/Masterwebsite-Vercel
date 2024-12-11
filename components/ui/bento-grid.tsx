fd647bfbf5c55dafa2819f5a23e78b8d4a44d585
Rishi1314 <rishiraj13052003@gmail.com>
2024-12-11 00:23:34 -0500

    Debugging Vercel(Grid(Background Beams))

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -75,139 +75,134 @@ export const BentoGridItem = ({
   };
 
   return (
-    <div>
-      Bento Grid Item
-      </div>
-
-  )
-
-  // return (
-  //   <div
-  //     className={cn(
-  //       // remove p-4 rounded-3xl dark:bg-black dark:border-white/[0.2] bg-white  border border-transparent, add border border-white/[0.1] overflow-hidden relative
-  //       "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 ",
-  //       className
-  //     )}
+    <div
+      className={cn(
+        // remove p-4 rounded-3xl dark:bg-black dark:border-white/[0.2] bg-white  border border-transparent, add border border-white/[0.1] overflow-hidden relative
+        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 ",
+        className
+      )}
       
-  //     style={id === 4 ? {background:"rgba(254, 139, 38, 0.8)"} :id === 5 ? {background:"rgba(29, 29, 29, 0.97)"}:id===3?{background:"#FEF9F2",border:"solid 3px grey"}:{
-  //       background: "rgb(43,43,43)",
-  //       backgroundColor: "radial-gradient(circle, rgba(43,43,43,1) 65%, rgba(94,94,95,1) 83%, rgba(137,137,138,0.9893207282913166) 100%)"
-  //     }}
-  //   >
-  //     {/* add img divs */}
-  //     {id===5&&(<BackgroundBeams />
-  //         )
-  //         }
-  //     <div className={`${id === 6 && "flex justify-center"} h-full`}>
-  //       <div className="w-full h-full absolute">
-  //         {img && (
-  //           <img
-  //             src={img}
-  //             alt={img}
-  //             className={cn(imgClassName, "object-cover object-center ")}
-  //           />
-  //         )}
-  //       </div>
-  //       <div
-  //         className={`absolute right-0 -bottom-5 ${id === 5 && "w-full opacity-80"
-  //           } `}
-  //       >
-  //         {spareImg && (
-  //           <img
-  //             src={spareImg}
-  //             alt={spareImg}
-  //             //   width={220}
-  //             className="object-cover object-center w-full h-full"
-  //           />
-  //         )}
-  //       </div>
-  //       {id === 6 && (
-  //         // add background animation , remove the p tag
-  //           <BackgroundGradientAnimation>
-  //                         <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
+      style={id === 4 ? {background:"rgba(254, 139, 38, 0.8)"} :id === 5 ? {background:"rgba(29, 29, 29, 0.97)"}:id===3?{background:"#FEF9F2",border:"solid 3px grey"}:{
+        background: "rgb(43,43,43)",
+        backgroundColor: "radial-gradient(circle, rgba(43,43,43,1) 65%, rgba(94,94,95,1) 83%, rgba(137,137,138,0.9893207282913166) 100%)"
+      }}
+    >
+      {/* add img divs */}
+      {id === 5 && (
+        // <BackgroundBeams />
+        <div>Background Beams</div>
+          )
+          }
+      <div className={`${id === 6 && "flex justify-center"} h-full`}>
+        <div className="w-full h-full absolute">
+          {img && (
+            <img
+              src={img}
+              alt={img}
+              className={cn(imgClassName, "object-cover object-center ")}
+            />
+          )}
+        </div>
+        <div
+          className={`absolute right-0 -bottom-5 ${id === 5 && "w-full opacity-80"
+            } `}
+        >
+          {spareImg && (
+            <img
+              src={spareImg}
+              alt={spareImg}
+              //   width={220}
+              className="object-cover object-center w-full h-full"
+            />
+          )}
+        </div>
+        {id === 6 && (
+          // add background animation , remove the p tag
+            <BackgroundGradientAnimation>
+                          <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
 
-  //           </BackgroundGradientAnimation>
+            </BackgroundGradientAnimation>
             
           
-  //       )}
+        )}
 
-  //       <div
-  //         className={cn(
-  //           titleClassName,
-  //           "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
-  //         )}
-  //       >
-  //         {/* change the order of the title and des, font-extralight, remove text-xs text-neutral-600 dark:text-neutral-300 , change the text-color */}
-  //         <div className={`font-sans  md:max-w-32 md:text-xs lg:text-lg text-sm ${id===3?"text-black-200":"text-[#C1C2D3] font-extralight"} z-10 `}>
-  //           {description}
-  //         </div>
-  //         {/* add text-3xl max-w-96 , remove text-neutral-600 dark:text-neutral-300*/}
-  //         {/* remove mb-2 mt-2 */}
-  //         <div
-  //           className={`font-sans z-10`}
-  //         >
-  //           {title}
-  //         </div>
+        <div
+          className={cn(
+            titleClassName,
+            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+          )}
+        >
+          {/* change the order of the title and des, font-extralight, remove text-xs text-neutral-600 dark:text-neutral-300 , change the text-color */}
+          <div className={`font-sans  md:max-w-32 md:text-xs lg:text-lg text-sm ${id===3?"text-black-200":"text-[#C1C2D3] font-extralight"} z-10 `}>
+            {description}
+          </div>
+          {/* add text-3xl max-w-96 , remove text-neutral-600 dark:text-neutral-300*/}
+          {/* remove mb-2 mt-2 */}
+          <div
+            className={`font-sans z-10`}
+          >
+            {title}
+          </div>
 
-  //         {/* for the github 3d globe */}
-  //         {id === 2}
+          {/* for the github 3d globe */}
+          {id === 2}
 
-  //         {/* Tech stack list div */}
-  //         {id === 3 && (
-  //           <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
-  //             {/* tech stack lists */}
-  //             <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
-  //               {leftLists.map((item, i) => (
-  //                 <span
-  //                   key={i}
-  //                   className="lg:py-4 lg:px-3 text-white py-2 px-3 text-xs lg:text-base opacity-50 
-  //                   lg:opacity-100 rounded-lg text-center bg-[#5280f69c]"
-  //                 >
-  //                   {item}
-  //                 </span>
-  //               ))}
-  //               <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center r bg-[#5280f69c"></span>
-  //             </div>
-  //             <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
-  //               <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#c44af9cb]"></span>
-  //               {rightLists.map((item, i) => (
-  //                 <span
-  //                   key={i}
-  //                   className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
-  //                   lg:opacity-100 rounded-lg text-center text-white bg-[#c44af9cb]"
-  //                 >
-  //                   {item}
-  //                 </span>
-  //               ))}
-  //             </div>
-  //           </div>
-  //         )}
-  //         {id === 6 && (
-  //           <div className="mt-5  relative">
-  //             {/* button border magic from tailwind css buttons  */}
-  //             {/* add rounded-md h-8 md:h-8, remove rounded-full */}
-  //             {/* remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 */}
-  //             {/* add handleCopy() for the copy the text */}
-  //             <div
-  //               className={`absolute -bottom-5 right-0 ${copied ? "block" : "block"
-  //                 }`}
-  //             >
-  //               {/* <img src="/confetti.gif" alt="confetti" /> */}
-  //               <Lottie options={defaultOptions} height={200} width={400} />
-  //             </div>
+          {/* Tech stack list div */}
+          {id === 3 && (
+            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+              {/* tech stack lists */}
+              <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+                {leftLists.map((item, i) => (
+                  <span
+                    key={i}
+                    className="lg:py-4 lg:px-3 text-white py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#5280f69c]"
+                  >
+                    {item}
+                  </span>
+                ))}
+                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center r bg-[#5280f69c"></span>
+              </div>
+              <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#c44af9cb]"></span>
+                {rightLists.map((item, i) => (
+                  <span
+                    key={i}
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center text-white bg-[#c44af9cb]"
+                  >
+                    {item}
+                  </span>
+                ))}
+              </div>
+            </div>
+          )}
+          {id === 6 && (
+            <div className="mt-5  relative">
+              {/* button border magic from tailwind css buttons  */}
+              {/* add rounded-md h-8 md:h-8, remove rounded-full */}
+              {/* remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 */}
+              {/* add handleCopy() for the copy the text */}
+              <div
+                className={`absolute -bottom-5 right-0 ${copied ? "block" : "block"
+                  }`}
+              >
+                {/* <img src="/confetti.gif" alt="confetti" /> */}
+                <Lottie options={defaultOptions} height={200} width={400} />
+              </div>
 
-  //             <MagicButton
-  //               title={copied ? "Email is Copied!" : "Copy my email address"}
-  //               icon={<IoCopyOutline />}
-  //               position="left"
-  //               handleClick={handleCopy}
-  //               otherClasses="!bg-[#161A31]"
-  //             />
-  //           </div>
-  //         )}
+              <MagicButton
+                title={copied ? "Email is Copied!" : "Copy my email address"}
+                icon={<IoCopyOutline />}
+                position="left"
+                handleClick={handleCopy}
+                otherClasses="!bg-[#161A31]"
+              />
+            </div>
+          )}
          
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
+        </div>
+      </div>
+    </div>
+  );
 };
