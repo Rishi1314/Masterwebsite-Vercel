e178894c9df3cfca500353bdda0e9c2d972014e5
Rishi1314 <rishiraj13052003@gmail.com>
2025-01-16 23:45:19 -0500

    Working on project card

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,14 +6,15 @@ const data = [
     {
         title: "ToShare",
         date: "2023",
-        abstract:"Simple File Sharing.",
-        imageUrl:"/tosharephoto.png"
+        abstract:"Built ToShare, a web app that makes decentralized file sharing simple and fun. It uses IPFS and Web3Storage for secure storage and lets you retrieve files with easy-to-remember word-based strings no long links or hassle. The user-friendly design keeps everything straightforward, so sharing and accessing files feels effortless.",
+    imageUrl: "/tosharephoto.png",
+    techStack: ["ReactJS", "|", "ChakraUI", "|", "IPFS", "|", "Web3Storage", "|", "Google Firebase"],
       
     },
     
   ];
   return (
-      <div className="w-full flex bg-[#010206] gap-2  flex-col items-center">
+      <div className="w-full flex bg-[#010206]  gap-2  flex-col items-center">
           <div className='text-white text-4xl font-bold' >My Projects</div>
       <ProjectWork data={data} />
     </div>
--- components/ui/projectWork.tsx
+++ components/ui/projectWork.tsx
@@ -9,31 +9,41 @@ interface ProjectEntry {
   date?: string;
   abstract: string;
   imageUrl: string;
+  techStack: Array<string>;
   // content: React.ReactNode;
 }
 
 const projectWork = ({ data }: { data: ProjectEntry[] }) => {
   return (
-    <div className='bg-[#151515] w-[80vw] p-2 rounded-[10px]'>
+    <div className='bg-[#151515] w-[90vw] lg-w-[80vw] p-2 rounded-[10px]'>
       {
         data.map((item, index) => (
-          <div key={index}>
-            <div className='w-full flex justify-between'>
-              <span className='text-3xl flex items-center'>{item.title}<a href='https://toshare.vercel.app' target='_blank' className='text-blue-300 hover:scale-110 cursor-pointer '><FaLink /></a></span>
-              <span className='text-2xl'>{item.date}</span>
+          <div key={index} className='flex flex-col gap-2'>
+            <div className='w-full flex justify-between px-4'>
+              <span className='lg-text-3xl flex items-center gap-2'>{item.title}<a href='https://toshare.vercel.app' target='_blank' className='text-blue-300 hover:scale-110 cursor-pointer '><FaLink /></a></span>
+              <span className='lg-text-2xl lg-font-bold'>{item.date}</span>
             </div>
-            <div className='w-full flex '>
+            <div className='w-full flex flex-col lg-flex-row '>
               <Image
-                          src={item.imageUrl}
-                          alt="startup template"
-                          width={500}
-                          height={500}
-                          className="hover:scale-125 cursor-pointer duration-200 rounded-lg object-cover h-20 md:h-44 lg:h-60 w-[30%] shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-                        />
-                            <div className='w-[70%] text-center'>{item.abstract}</div>
+                src={item.imageUrl}
+                alt="startup template"
+                width={500}
+                height={500}
+                className="hover:scale-125 cursor-pointer duration-200 rounded-lg object-cover h-20 md:h-44 lg:h-60 w-[30%] shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+              />
+              <div className='flex  flex-col w-full justify-center items-center'>
+                <div className='flex border-b-2 border-solid border-white text-xl gap-2'>
+                  {(item.techStack).map((i, k) => (
+                    <span key={k}>
+                      {i}
+                    </span>
+                  ))}
+                </div>
+              <div className='w-[70%] text-lg text-gray-400 text-center'>{item.abstract}</div>
 
+              </div>
             </div>
-            
+
           </div>
         ))
       }
