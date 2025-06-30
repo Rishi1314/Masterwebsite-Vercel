2892b2a821b7c630424eccf0c48b05a3128c0374
Rishi1314 <rishiraj13052003@gmail.com>
2025-06-30 23:37:16 +0530

    Minor Changes to Project Section.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,5 @@
 "use client";
- 
+
 import { motion } from "framer-motion";
 import Image from "next/image";
 import React from "react";
@@ -11,77 +11,77 @@ import MagicButton from "./MagicButton";
 // import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
-    const words = [
-        {
-          text: "Hi,",
-        },
-        {
-          text: "I",
-        },
-        {
-          text: "am",
-        },
-        {
-          text: "Rishi Raj",
-          className: " text-purple-300 dark:text-purple-300",
-        },
-      ];
+  const words = [
+    {
+      text: "Hi,",
+    },
+    {
+      text: "I",
+    },
+    {
+      text: "am",
+    },
+    {
+      text: "Rishi Raj",
+      className: " text-purple-300 dark:text-purple-300",
+    },
+  ];
   return (
     <div className="h-[50rem] text-center w-full dark:bg-gray-950 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] relative flex flex-col items-center justify-center">
-          {/* Radial gradient for the container to give a faded look */}
-          <Spotlight
+      {/* Radial gradient for the container to give a faded look */}
+      <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
-          />
-          
+      />
+
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <motion.div
         initial={{
           opacity: 0,
-          scale:0.2
+          scale: 0.2
         }}
         animate={{
-          scale:1,
-          opacity:  1,
+          scale: 1,
+          opacity: 1,
         }}
         transition={{
           duration: 1,
         }}
       >
-<Image
-                    src={mycartoon}
-                    alt="me"
-                    className="z-10 rounded-full bored border-solid border-[2px] border-gray-600 bg-[#706e6ec6] profilePhoto aspect-square w-[15rem]"
-                />
+        <Image
+          src={mycartoon}
+          alt="me"
+          className="z-10 rounded-full bored border-solid border-[2px] border-gray-600 bg-[#706e6ec6] profilePhoto aspect-square w-[15rem]"
+        />
       </motion.div>
-      
-          <div className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-4">
+
+      <div className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-4">
         <TypewriterEffectSmooth className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8" words={words} />
 
       </div>
-      <motion.p 
+      <motion.p
         initial={{
           opacity: 0,
-          y:100
+          y: 100
         }}
         animate={{
-          opacity:1,
-          y:0
+          opacity: 1,
+          y: 0
         }}
         transition={{
           duration: 1,
         }}
         className="text-xl sm:text-xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500">
         Wanna work together? here is <span>
-        <a href="https://drive.google.com/file/d/1j2sxL-gNJj_Cx0mySKfIzcg7ewXnzitq/view?usp=drive_link" target="_blank">
+          <a href="https://drive.google.com/file/d/1j2sxL-gNJj_Cx0mySKfIzcg7ewXnzitq/view?usp=drive_link" target="_blank">
             <MagicButton
               title="my Resume"
               position="right"
             />
           </a>
-      </span>
+        </span>
       </motion.p>
-    </div>  )
+    </div>)
 }
 
 export default Hero
--- components/ui/projectWork.tsx
+++ components/ui/projectWork.tsx
@@ -15,21 +15,21 @@ interface ProjectEntry {
 
 const projectWork = ({ data }: { data: ProjectEntry[] }) => {
   return (
-    <div className='bg-[#151515] w-[90vw] lg-w-[80vw] p-2 rounded-[10px]'>
+    <div className='bg-[#151515] w-[60vw] p-2 rounded-[10px]'>
       {
         data.map((item, index) => (
           <div key={index} className='flex flex-col gap-2'>
             <div className='w-full flex justify-between px-4'>
-              <span className='lg-text-3xl flex items-center gap-2'>{item.title}<a href='https://toshare.vercel.app' target='_blank' className='text-blue-300 hover:scale-110 cursor-pointer '><FaLink /></a></span>
-              <span className='lg-text-2xl lg-font-bold'>{item.date}</span>
+              <span className='text-3xl flex items-center gap-2 cursor-pointer'>{item.title}<a href='https://toshare.vercel.app' target='_blank' className='text-blue-300 hover:scale-110 cursor-pointer '><FaLink /></a></span>
+              <span className='text-2xl font-bold'>{item.date}</span>
             </div>
-            <div className='w-full flex flex-col lg-flex-row '>
+            <div className='w-full gap-2 items-center justify-center flex flex-col lg-flex-row '>
               <Image
                 src={item.imageUrl}
                 alt="startup template"
                 width={500}
                 height={500}
-                className="hover:scale-125 cursor-pointer duration-200 rounded-lg object-cover h-20 md:h-44 lg:h-60 w-[30%] shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+                className="hover:scale-125 cursor-pointer duration-200 rounded-lg object-cover h-20 md:h-44 lg:h-60 w-[40%] shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
               />
               <div className='flex  flex-col w-full justify-center items-center'>
                 <div className='flex border-b-2 border-solid border-white text-xl gap-2'>
@@ -39,7 +39,7 @@ const projectWork = ({ data }: { data: ProjectEntry[] }) => {
                     </span>
                   ))}
                 </div>
-              <div className='w-[70%] text-lg text-gray-400 text-center'>{item.abstract}</div>
+                <div className='w-[70%] text-lg text-gray-400 text-center'>{item.abstract}</div>
 
               </div>
             </div>
